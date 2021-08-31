a1c3dabc4fd22d3629c82fffe836321c304d3ebc
Sofia <molinerozadkiel@gmail.com>
2021-08-31 18:53:11 +0200

    mejora pequeña en proyectos

--- page-proyectos.php
+++ page-proyectos.php
@@ -16,7 +16,7 @@
     );
     $banners=new WP_Query($args);
     while($banners->have_posts()){$banners->the_post();?>
-      <div class="blah">
+      <a class="blah" href="<?= get_permalink($post) ?>">
         <img class="blah_img" loading="lazy" src="<?php the_post_thumbnail_url(); ?>" alt="">
         <div class="blah_caption">
           <p class="blah_type">Diseño UX UI</p>
@@ -24,7 +24,7 @@
           <div class="blah_deco"></div>
           <p class="blah_text">No te conformes. Hazte independiente. Crea una web escalable, capaz de crecer a la par de tu empresa.</p>
         </div>
-      </div>
+      </a>
     <?php } wp_reset_query(); ?>
   </div>
 
